b186bce086f8c045d84712b10c7ae7f5db2c29de
OZ <normandiggs@gmail.com>
2015-09-28 20:25:27 +0300

    Customizable button icons.

--- pagination.js
+++ pagination.js
@@ -3,18 +3,22 @@ angular.module('oz.mdPagination', []).directive('ozMdPagination', function () {
   return {
     restrict: 'E',
     template: '<div layout="row">' +
-    '<md-button flex="10" ng-click="prev()"><span class="ion-arrow-left-b"></span></md-button>' +
+    '<md-button flex="10" ng-click="prev()"><span ng-class="leftIcon"></span></md-button>' +
     '<md-slider flex ng-model="ngModel" min="{{min}}" max="{{max}}" ng-class="sliderClass"></md-slider>' +
-    '<md-button flex="10" ng-click="next()"><span class="ion-arrow-right-b"></span></md-button>' +
+    '<md-button flex="10" ng-click="next()"><span ng-class="rightIcon"></span></md-button>' +
     '</div>',
     scope: {
       ngModel: '=',
       min: '@',
       max: '@',
       step: '@',
-      sliderClass: '@'
+      sliderClass: '@',
+      leftIcon: '@',
+      rightIcon: '@'
     },
     link: function (scope) {
+      scope.leftIcon = scope.leftIcon || 'ion-arrow-left-b';
+      scope.rightIcon = scope.rightIcon || 'ion-arrow-right-b';
       scope.next = function () {
         scope.ngModel = parseInt(scope.ngModel) + parseInt(scope.step);
         if (scope.ngModel > scope.max) {
